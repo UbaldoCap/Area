f21d8e146a3cd1d645ae1911b24a400bd2a33d23
Ubaldo <ubaldocapalbo@gmail.com>
2023-11-23 14:32:46 +0100

    end

--- Forma.java
+++ Forma.java
@@ -1,6 +1,28 @@
 public class Forma {
-    public Forma() {
+    private int x;
+    private int y;
+
+    public Forma(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public void calcolaArea() {
         System.out.print("area = ");
     }
--- Rettangolo.java
+++ Rettangolo.java
@@ -1,31 +1,11 @@
 public class Rettangolo extends  Forma {
-    private int base;
-    private int altezza;
-
-    public Rettangolo(int base, int altezza) {
-        this.base = base;
-        this.altezza = altezza;
-    }
-
-    public int getBase() {
-        return base;
-    }
-
-    public void setBase(int base) {
-        this.base = base;
-    }
-
-    public int getAltezza() {
-        return altezza;
-    }
-
-    public void setAltezza(int altezza) {
-        this.altezza = altezza;
+    public Rettangolo(int x, int y) {
+        super(x, y);
     }
 
     @Override
     public void calcolaArea() {
         super.calcolaArea();
-        System.out.println(base * altezza);
+        System.out.println(getX() * getY());
     }
 }
